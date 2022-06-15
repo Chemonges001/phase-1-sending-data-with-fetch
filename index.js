15472c7091380f4939480892dad95b0ccfa6f114
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-16 00:00:27 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData(name, email) {
+    
+    const userInfo = {name: name, email: email}
+
+    return fetch("http://localhost:3000/users",{
+    method : "POST",
+    headers : {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
+
+    body: JSON.stringify(userInfo),
+
+    })
+
+    .then (response => response.json())
+    .then(users => {
+        document.body.innerHTML = users.id
+    })
+
+    .catch(function (error) {
+        document.body.innerHTML = error.message;
+     })
+     
+}
+
